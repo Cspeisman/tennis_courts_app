f176c703fc2e8ed78d1c28c4c63cebc1a77ec4b1
Cspeisman <Cspeisman@gmail.com>
2014-11-29 17:09:52 -0500

    whoops took out console log

--- app/controllers/courts_controller.js
+++ app/controllers/courts_controller.js
@@ -13,11 +13,7 @@ module.exports = {
       model: {model: 'Court', params: params}
     };
     this.app.fetch(spec, function(err, result) {
-      // We must check for an error before accessing `result.model` (below),
-      // which may be undefined if there's an error (404, 500, etc.).
-      if (err) return callback(err);
-
-      callback(null, result);
-    }.bind(this));
+      callback(err, 'courts/show', result)
+    });
   }
 };
